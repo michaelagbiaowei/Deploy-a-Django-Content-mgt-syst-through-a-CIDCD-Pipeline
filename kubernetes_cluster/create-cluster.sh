17676c6980e45d4a1ed7ea5e98ab430d78af4c5b
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-06 16:01:23 +0000

    updated

--- kubernetes_cluster/create-cluster.sh
+++ kubernetes_cluster/create-cluster.sh
@@ -4,10 +4,11 @@
 
 # Step 1:
 # Create the database-service cluster
-kubectl apply -f configmap.yml
-kubectl apply -f storage.yml
-kubectl apply -f postgres-deployment.yml
-kubectl apply -f service.yml 
+# kubectl apply -f configmap.yml
+# kubectl apply -f storage.yml
+# kubectl apply -f postgres-deployment.yml
+# kubectl apply -f service.yml 
+kubectl apply -f postdb.yml
 
 # Step 2:
 # Create the deployment-service cluster
