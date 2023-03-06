e7b7265e6d0f844c4aa37e9f5edb5faf1fefbe25
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-06 22:58:41 +0000

    updated

--- kubernetes_cluster/create-cluster.sh
+++ kubernetes_cluster/create-cluster.sh
@@ -4,13 +4,13 @@
 
 # Step 1:
 # Create the database-service cluster
-# kubectl apply -f configmap.yml
-# kubectl apply -f storage.yml
+kubectl apply -f configmap.yml
+kubectl apply -f storage.yml
 # kubectl apply -f database.yml
-kubectl apply -f test.yml
-# kubectl apply -f service.yml 
+kubectl apply -f t.yml
+kubectl apply -f service.yml 
 # # kubectl apply -f postdb.yml
 
 # Step 2:
 # Create the deployment-service cluster
-# kubectl apply -f django-app-deployment.yml
+kubectl apply -f django-app-deployment.yml
