3a34dbafb82f3259ac7de09e62788f5225ac9d92
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-07 23:05:56 +0000

    updated

--- kubernetes_cluster/create-cluster.sh
+++ kubernetes_cluster/create-cluster.sh
@@ -3,14 +3,15 @@
 #!/usr/bin/env bash
 
 # Step 1:
-# Create the database-service cluster
-kubectl apply -f configmap.yml
-kubectl apply -f storage.yml
-# kubectl apply -f database.yml
-kubectl apply -f t.yml
-kubectl apply -f service.yml 
-# # kubectl apply -f postdb.yml
-
+# # Create the database-service cluster
+# kubectl apply -f configmap.yml
+# kubectl apply -f storage.yml
+# # kubectl apply -f database.yml
+# kubectl apply -f t.yml
+# kubectl apply -f service.yml 
+# # # kubectl apply -f postdb.yml
+kubectl apply -f Deployment.app.db.yml
 # Step 2:
 # Create the deployment-service cluster
-kubectl apply -f django-app-deployment.yml
+# kubectl apply -f django-app-deployment.yml
+# I'm trying to deploy an application on Kubernetes.
