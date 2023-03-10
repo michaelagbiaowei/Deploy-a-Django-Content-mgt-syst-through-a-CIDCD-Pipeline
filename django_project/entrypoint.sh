f7377c8def891a02f0744a726d366985926052a3
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-10 13:31:22 +0100

    Update entrypoint.sh

--- django_project/entrypoint.sh
+++ django_project/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # pipenv shell
 pipenv run python3 /app/manage.py migrate
-pipenv run python3 /app/manage.py runserver
+pipenv run python3 /app/manage.py runserver 0.0.0.0:8000
 exec "$@"
