c8f9182a41f3bf2003dc1a1037f8bd381dbf157d
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-20 13:50:18 +0100

    Agbiaowei Ifiekemi

--- scripts/minikube-install.sh
+++ scripts/minikube-install.sh
@@ -13,10 +13,10 @@ minikube version
 
 # Step 3:
 # Start the kubernetes cluster
-minikube start # Comment this line if you are not UBUNTU
+minikube start # Comment this line if you are not using UBUNTU
 # sudo minikube start --force    # Uncomment this line if you are using UBUNTU
 
 # Step 4:
 # List kubernetes pods
-minikube kubectl -- get pods -A # Comment this line if you are using online UBUNTU
+minikube kubectl -- get pods -A # Comment this line if you are not using online UBUNTU
 # sudo minikube kubectl -- get pods -A  # Uncomment this line if you are using UBUNTU
