6e1b16f0f9093212872355db0a8eb4b403211d02
Ima-Abasi E. Effiong <imaabasieea@gmail.com>
2023-03-22 13:04:28 +0000

    uptaed

---
+++ node-project/upload_docker.sh
@@ -0,0 +1,22 @@
+# USING BASH SCRIPT TO TAG AND UPLOAD AN IMAGE TO DOCKER HUB
+
+#!/usr/bin/env bash
+
+# Assumes that an image is built via `run_docker-compose.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=nodejs
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u maiempire
+
+#Step 3:
+# Tag the images with your Docker ID
+docker tag $dockerpath:latest maiempire/$dockerpath
+
+# Step 4:
+# Push image to a docker repository
+docker push maiempire/$dockerpath
